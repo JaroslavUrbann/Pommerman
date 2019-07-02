b5a49b9cd4836ad8b1e8b237531bdadddfb1e3b6
jaroslav <jaroslav.urbann@gmail.com>
2019-07-02 18:51:14 +0200

    initial architecture done

--- large_network.py
+++ large_network.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import time
 
 
 class LargeNetwork:
@@ -12,93 +13,63 @@ class LargeNetwork:
     n_map_features = 14
     n_player_features = 1
 
-    def model(self, x1, x2):
-        weights = {
-            'W_conv1': tf.Variable(tf.random_normal([3, 3, self.n_map_features, 128])),
-            'W_conv2': tf.Variable(tf.random_normal([3, 3, 128, 128])),
-            'W_conv3': tf.Variable(tf.random_normal([3, 3, 128, 64])),
-            'W_conv4': tf.Variable(tf.random_normal([3, 3, 64, 64])),
-            'W_fc1': tf.Variable(tf.random_normal([11 * 11 * (self.n_map_features + self.n_player_features), 2048])),
-            'W_fc2': tf.Variable(tf.random_normal([2048, 512])),
-            'W_fc3': tf.Variable(tf.random_normal([512, 128])),
-            'out': tf.Variable(tf.random_normal([128, self.n_classes])),
-        }
+    def initialize_model(self):
+        x1_map = tf.keras.layers.Input(shape=(11, 11, self.n_map_features))
+        x2_map = tf.keras.layers.Input(shape=(11, 11, self.n_map_features))
+        x1_player = tf.keras.layers.Input(shape=(11, 11, self.n_player_features))
+        x2_player = tf.keras.layers.Input(shape=(11, 11, self.n_player_features))
 
-        biases = {
-            'b_conv1': tf.Variable(tf.random_normal([128])),
-            'b_conv2': tf.Variable(tf.random_normal([128])),
-            'b_conv3': tf.Variable(tf.random_normal([64])),
-            'b_conv4': tf.Variable(tf.random_normal([64])),
-            'b_f1': tf.Variable(tf.random_normal([2048])),
-            'b_fc2': tf.Variable(tf.random_normal([512])),
-            'b_fc3': tf.Variable(tf.random_normal([128])),
-            'out': tf.Variable(tf.random_normal([self.n_classes]))
-        }
+        conv1 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
+        conv2 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
+        conv3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
+        conv4 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
+        conv5 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
+        conv6 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")
 
-        def _conv2d(x, W, b, strides=1):
-            x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
-            x = tf.nn.bias_add(x, b)
-            return tf.nn.relu(x)
+        x1_latent = conv6(conv5(conv4(conv3(conv2(conv1(x1_map))))))
+        x2_latent = conv6(conv5(conv4(conv3(conv2(conv1(x2_map))))))
 
-        # Reshape input to a 4D tensor
-        x1 = tf.reshape(x1, shape=[-1, 11, 11, self.n_map_features])
-        x2 = tf.reshape(x2, shape=[-1, 11, 11, self.n_player_features])
+        merged = tf.keras.layers.concatenate([x1_latent, x1_player, x2_latent, x2_player], axis=3)
 
-        conv1 = _conv2d(x1, weights['W_conv1'], biases['b_conv1'])
-        conv2 = _conv2d(conv1, weights['W_conv2'], biases['b_conv2'])
-        conv3 = _conv2d(conv2, weights['W_conv3'], biases['b_conv3'])
-        conv4 = _conv2d(conv3, weights['W_conv4'], biases['b_conv4'])
-        concat = tf.concat([conv4, x2], 3)
-        reshaped = tf.reshape(concat, [-1, 11 * 11 * (self.n_map_features + self.n_player_features)])
-        fc1 = tf.add(tf.matmul(reshaped, weights['W_fc1']), biases['b_f1'])
-        fc2 = tf.add(tf.matmul(fc1, weights['W_fc2']), biases['b_f2'])
-        fc3 = tf.add(tf.matmul(fc2, weights['W_fc3']), biases['b_f3'])
-        out = tf.add(tf.matmul(fc3, weights['out']), biases['out'])
+        conv7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")(merged)
+        conv8 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")(conv7)
+        conv9 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")(conv8)
+        conv10 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")(conv9)
+        conv11 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding="same")(conv10)
 
-        return out
+        flat = tf.keras.layers.Flatten()(conv11)
 
-    def run_model(self):
-        # tf Graph input
-        x1 = tf.placeholder(tf.float32, [None, 11, 11, self.n_map_features])
-        x2 = tf.placeholder(tf.float32, [None, 11, 11, self.n_player_features])
-        y = tf.placeholder(tf.float32, [None, self.n_classes])
+        fc1 = tf.keras.layers.Dense(4096, activation='relu')(flat)
+        fc2 = tf.keras.layers.Dense(1024, activation='relu')(fc1)
+        fc3 = tf.keras.layers.Dense(512, activation='relu')(fc2)
+        fc1 = tf.keras.layers.Dense(128, activation='relu')(flat)
+        fc1 = tf.keras.layers.Dense(32, activation='relu')(flat)
+        out = tf.keras.layers.Dense(self.n_classes, activation='softmax')(fc3)
 
-        # Model
-        logits = self.model(x1, x2)
+        model = tf.keras.models.Model(inputs=[x1_map, x2_map, x1_player, x2_player], outputs=out)
+        model.compile(loss="categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(lr=self.lr))
+        self.model = model
+        model.summary()
 
-        # Define loss and optimizer
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.lr).minimize(cost)
+    def train_model(self):
+        x1_map = np.ones(shape=(64, 11, 11, self.n_map_features))
+        x2_map = np.ones(shape=(64, 11, 11, self.n_map_features))
+        x1_player = np.ones(shape=(64, 11, 11, self.n_player_features))
+        x2_player = np.ones(shape=(64, 11, 11, self.n_player_features))
+        y = np.ones(shape=(64, self.n_classes))
+        self.model.fit([x1_map, x2_map, x1_player, x2_player], y, epochs=2)
 
-        # Accuracy
-        correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-        # Initializing the variables
-        init = tf.global_variables_initializer()
-
-        # Launch the graph
-        with tf.Session() as sess:
-            sess.run(init)
-
-            for epoch in range(self.n_epochs):
-                for batch in range(self.n_batches):
-                    batch_x1 = np.zeros((self.batch_size, 11, 11, self.n_map_features))
-                    batch_x2 = np.zeros((self.batch_size, 11, 11, self.n_player_features))
-                    batch_y = np.zeros(self.n_classes)
-                    sess.run(optimizer, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-
-                    # Calculate batch loss and accuracy
-                    loss = sess.run(cost, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-                    valid_acc = sess.run(accuracy, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-
-                    print('Epoch {:>2}, Batch {:>3} - Loss: {:>10.4f} Validation Accuracy: {:.6f}'.format(
-                        epoch + 1,
-                        batch + 1,
-                        loss,
-                        valid_acc))
-
-            # Calculate Test Accuracy
+    def test_model(self):
+        x1_map = np.random.rand(1, 11, 11, self.n_map_features).astype("float32")
+        x2_map = np.random.rand(1, 11, 11, self.n_map_features).astype("float32")
+        x1_player = np.random.rand(1, 11, 11, self.n_player_features).astype("float32")
+        x2_player = np.random.rand(1, 11, 11, self.n_player_features).astype("float32")
+        tim = time.time()
+        print(self.model([x1_map, x2_map, x1_player, x2_player]))
+        print(time.time() - tim)
 
 
-LargeNetwork().run_model()
+LN = LargeNetwork()
+LN.initialize_model()
+LN.train_model()
+# LN.test_model()
