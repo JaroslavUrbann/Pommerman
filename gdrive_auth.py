9abca27c97798f3e67aee48036c26af3108ea2fa
jaroslav <jaroslav.urbann@gmail.com>
2019-07-06 13:13:13 +0200

    refreshing google auth

--- gdrive_auth.py
+++ gdrive_auth.py
@@ -2,11 +2,27 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
+import time
+from threading import Thread
+
+
+gauth = None
+
+
+def refresh_token():
+    global gauth
+    while True:
+        time.sleep(45 * 60)
+        if gauth.access_token_expired:
+            gauth.Refresh()
 
 
 # google colab only
 def get_drive():
+    global gauth
     auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
+    refresh = Thread(target=refresh_token, daemon=True)
+    refresh.start()
     return GoogleDrive(gauth)
--- pretraining_main.py
+++ pretraining_main.py
@@ -8,16 +8,18 @@ import gdrive_auth
 
 def main():
     # subprocess.call('docker kill $(docker ps -q)', shell=True)
+    drive = gdrive_auth.get_drive()
     pretraining_database.create_database(db_size=50)
     LN = LargeNetwork()
-    LN.init_dummy_model()
+    LN.load_model(drive, id="1HPKpEAf0p9AOu7ePXGU19ivxCbMx4YKH")
+
+    # LN.init_dummy_model()
     LN.train_model_on_database(n_epochs=4)
     # LN.plot_history()
     # result = testing_game_env.test_network(n_games=4, LN=LN)
-    result = [3, 2, 1]
-    drive = gdrive_auth.get_drive()
-    LN.upload_logs(drive, name="test_ads", test_results=result)
-    LN.upload_model(drive, name="test_ads")
+    result = [3, 2, 5]
+    LN.upload_logs(drive, id="1u2YVOUtA6kxPWHm-SL5_zBRprOCNWKqT", name=None, test_results=result)
+    LN.upload_model(drive, name=None)
     # print(result)
 
 main()
