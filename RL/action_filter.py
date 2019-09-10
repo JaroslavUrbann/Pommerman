ed00da8b2ca35ba89bc6950c0db001bb4d8de49a
jaroslav <jaroslav.urbann@gmail.com>
2019-09-10 22:52:38 +0200

    tried to optimize action_filter

--- RL/action_filter.py
+++ RL/action_filter.py
@@ -5,11 +5,12 @@ from constants import *
 # check a square is surrounded by unmovable blocks
 # created to prevent agents from going into 1x1 traps after placing a bomb, that they can't kick
 def is_square_a_trap(row, col, features):
-    l = is_square_blocked(row, col, features, (0, -1))
-    d = is_square_blocked(row, col, features, (+1, 0))
-    r = is_square_blocked(row, col, features, (0, +1))
-    u = is_square_blocked(row, col, features, (-1, 0))
-    return l and d and r and u
+    if is_square_blocked(row, col, features, (0, -1)) and \
+        is_square_blocked(row, col, features, (+1, 0)) and \
+        is_square_blocked(row, col, features, (0, +1)) and \
+        is_square_blocked(row, col, features, (-1, 0)):
+        return True
+    return False
 
 
 # checks if a square is blocked (needs direction to check if you can move a bomb)
@@ -42,11 +43,11 @@ def apply_action_filter(action_filter, probabilities):
 
 
 def get_action_filter(row, col, features):
-    a0 = is_square_safe(row, col, features)
-    a1 = is_square_safe(row - 1, col, features)
-    a2 = is_square_safe(row + 1, col, features)
-    a3 = is_square_safe(row, col - 1, features)
-    a4 = is_square_safe(row, col + 1, features)
+    a0 = is_square_safe(row, col, features, (0, 0))
+    a1 = is_square_safe(row - 1, col, features, (-1, 0))
+    a2 = is_square_safe(row + 1, col, features, (+1, 0))
+    a3 = is_square_safe(row, col - 1, features, (0, -1))
+    a4 = is_square_safe(row, col + 1, features, (0, +1))
     a5 = 0 if features[0, row, col, 9] > 0 else 1
     # stop, up, down, left, right, place_bomb
     return [a0, a1, a2, a3, a4, a5]
@@ -56,8 +57,9 @@ def get_action_filter(row, col, features):
 # so if an agent chooses an action with a low survival probability, I will let him.
 # I am leaving this code here to perhaps implement a safe agent in the future,
 # that always chooses an action with survival probability of 1
-def is_square_safe(row, col, features):
-
+def is_square_safe(row_, col_, features, direction):
+    row = row_ + direction[0]
+    col = col_ + direction[1]
     # absolutely no chance of stepping on or surviving on this square section:
     # is it outside the map?
     if col < 0 or col > 10 or row < 0 or row > 10:
@@ -82,24 +84,15 @@ def is_square_safe(row, col, features):
         # the agent is standing on top of the bomb (it is safe to stay there)
         if features[0, row, col, 7] == 1:
             return 1
-        # if the agent isn't standing on top of the bomb (he is standing to the side of it)
-        # and can't kick -> not safe (can't go there)
-        if features[0, row, col, 7] != 1 and features[0, row, col, 30] == 0:
+        # if the agent can't kick -> not safe (can't go there)
+        if features[0, row, col, 30] == 0:
             return 0
         # if the agent is standing to the side of the bomb, can he push it? (is the square behind it safe?)
-        if row < 10 and features[0, row + 1, col, 7] == 1:
-            return is_square_safe(row - 1, col, features)
-        if col < 10 and features[0, row, col + 1, 7] == 1:
-            return is_square_safe(row, col - 1, features)
-        if row > 0 and features[0, row - 1, col, 7] == 1:
-            return is_square_safe(row + 1, col, features)
-        if col > 0 and features[0, row, col - 1, 7] == 1:
-            return is_square_safe(row, col + 1, features)
-        # else:  # there are 2 bombs after each other and the second one could be moving / start moving
-        #     return 1
+        # if there are 2 bombs after each other, this breaks, since you can't push two bombs at the same time
+        return is_square_safe(row, col, features, direction)
 
     # is square a trap?
-    if is_square_a_trap(row, col, features):
+    if features[0, row_, col_, 9] > 0 and features[0, row_, col_, 7] == 1 and is_square_a_trap(row, col, features):
         return 0
 
     # this will need reworking if I ever use it, since I changed the flame / blast strength system
