c746f9b73bafc6f301aeaa39871a23bd121bfc8a
jaroslav <jaroslav.urbann@gmail.com>
2019-09-13 11:32:31 +0200

    added upload function

--- pretraining/DB.py
+++ pretraining/DB.py
@@ -1,22 +1,24 @@
 from constants import *
 from feature_engineer import FeatureEngineer
 import numpy as np
+import h5py
 
 
 class DB:
-    def __init__(self, db_size):
-        self.size = db_size
+    def __init__(self, size, name="db"):
+        self.name = name
+        self.size = size
         self.step_index = 0
-        self.x = np.zeros((db_size, 11, 11, N_FEATURES), dtype="float32")
-        self.y = np.zeros((db_size,), dtype="float32")
+        self.x = np.zeros((size, 11, 11, N_FEATURES), dtype="float32")
+        self.y = np.zeros((size,), dtype="float32")
         self.feature_engineers = [FeatureEngineer(), FeatureEngineer(), FeatureEngineer(), FeatureEngineer()]
 
 
-    def get_database(self):
+    def get(self):
         return self.x, self.y
 
 
-    def set_database(self, x, y):
+    def set(self, x, y):
         self.x = x
         self.y = y
 
@@ -33,3 +35,15 @@ class DB:
                 self.x[self.step_index] = features
                 self.y[self.step_index] = actions[i]
                 self.step_index += 1
+
+
+    def upload(self, drive):
+        with h5py.File(self.name, "w") as storage:
+            x_db = storage.create_dataset("x", self.x.shape, compression="gzip")
+            y_db = storage.create_dataset("y", self.y.shape, compression="gzip")
+            x_db[:, :, :, :] = self.x
+            y_db[:] = self.y
+
+        database = drive.CreateFile({'title': self.name})
+        database.SetContentFile(self.name)
+        database.Upload()
